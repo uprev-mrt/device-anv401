a18176e8e6764eb7bdc4486ec892535cd54da016
Jason <JBerger@up-rev.com>
2021-10-14 11:24:31 -0400

    saving

--- anv401.h
+++ anv401.h
@@ -18,6 +18,10 @@ extern "C"
 
 #define ANV401_HEAD 0xf5 
 #define ANV401_TAIL 0xf5 
+
+#define ANV401_CMD_OFFSET 1   
+#define ANV401_STATUS_OFFSET 4  //Only valid for 8 byte transactions
+
 /* Exported types ------------------------------------------------------------*/
 
 typedef enum{
@@ -44,6 +48,7 @@ typedef enum{
   ANV401_CMD_TIMEOUT  = 0x2E
 } anv401_cmd_e;
 
+
 typedef struct{
   mrt_uart_handle_t mUart;  //uart Handle
   mrt_gpio_t mIrq;          //IRQ/wake line
@@ -51,16 +56,48 @@ typedef struct{
 
 /* Exported constants --------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-anv401_status_e anv401_get_user_count(anv401_t* dev);
+
+/**
+ * @brief Initialize device
+ * @param dev ptr to device struct
+ * @param uart uart handle for device
+ * @param irq gpio handle for interrupt pin
+ */
+void anv401_init(anv401_t* dev, mrt_uart_handle_t uart, mrt_gpio_t irq);
+
+/**
+ * @brief Deinitialize device and free any resources
+ * @param dev ptr to device struct
+ */
+void anv401_deinit(anv401_t* dev);
+
+/**
+ * @brief Handles an inerrupt from the fingerprint sensor. This should be called on the Falling edge of the IRQ line
+ * @param dev ptr to device struct
+ * @return anv401_status_e 
+ */
+anv401_status_e anv401_handle_irq(anv401_t* dev);
+
+/**
+ * @brief Gets the number of existing fingerprints in db
+ * @param dev ptr to device struct
+ * @return number of fingerprints, or -1 if there is an error
+ */
+int anv401_get_user_count(anv401_t* dev);
+
+/**
+ * @brief Registers a fingerprint
+ * 
+ * @param dev ptr to device struct
+ * @return anv401_status_e 
+ */
+anv401_status_e anv401_add_user(anv401_t* dev);
+
 anv401_status_e anv401_get_compare_level(anv401_t* dev);
 anv401_status_e anv401_set_compare_level( anv401_t* dev ,uint8_t temp);	// The default value is 5, can be set to 0-9, the bigger, the stricter
-anv401_status_e anv401_add_user(anv401_t* dev);
 anv401_status_e anv401_clear_all_users(anv401_t* dev);
 anv401_status_e anv401_verify_user(anv401_t* dev);
 anv401_status_e anv401_get_timeout(anv401_t* dev);	
-void Analysis_PC_Command(void);
-void Auto_Verify_Finger(void);
-
 
 
 #ifdef __cplusplus
